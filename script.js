36d19baf5ea2b19b81b40ded6f3d606508cf7738
Oktazz <oktadwi7611@gmail.com>
2025-05-30 14:59:17 +0800

    active navbar

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelectorAll('.wisata-item').forEach(item => {
     });
 });
 
-// animasi 
+// animasi scroll
 function revealOnScroll() {
   const reveals = document.querySelectorAll('#sejarah, #kondisi, #tradisi, #wisata');
 
@@ -51,3 +51,26 @@ function updateHeading(){
 updateHeading();
 
 window.addEventListener('resize', updateHeading);
+
+// navbar active state
+const sections = document.querySelectorAll('section');
+const navLinks = document.querySelectorAll('.nav-link');
+
+window.addEventListener('scroll', () => {
+  let current = '';
+
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute('id');
+    }
+  });
+
+  navLinks.forEach(link => {
+    link.classList.remove('active');
+    if (link.getAttribute('href') === `#${current}`) {
+      link.classList.add('active');
+    }
+  });
+});
